ca3eed307c0b0e924e741d59211de024e96239eb
Jordan Cooper <jordan@cooperplanet.com>
2020-04-13 11:35:06 -0700

    Do it again

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Very hello again, world!'))
+app.get('/', (req, res) => res.send('Wow, very hello again, world!'))
 
+
+console.log("Hello console world!")
 app.listen(port, () => console.log(`Listening on port ${port}`))
